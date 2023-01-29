cdf2ae763b64f4f9e3a76234262bf929ed681167
scutayan <113755999+scutayan@users.noreply.github.com>
2023-01-29 17:41:53 +0800

    Add files via upload
    Loader

---
+++ src/screen/loader/loader.js
@@ -0,0 +1,19 @@
+import {useState} from 'react'
+import './loader.css'
+import './load.css'
+
+ export const Load = (props) => {
+    const [load, setLoad] = useState("loader-screen show")
+    
+    setTimeout(() => {
+      setLoad("loader-screen hide")
+    },props.time);
+   return (
+    <div className={load}>
+     <p>Loading</p>
+     <div class="shapes"></div>
+
+    
+    </div>
+   )
+ }
